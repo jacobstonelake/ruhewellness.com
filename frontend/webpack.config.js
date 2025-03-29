196153a18b72da07cadf729f202e5122e4bbd052
Jacob Stonelake <jstonelake9@gmail.com>
2025-03-29 15:49:35 -0400

    Fix: copy public folder to include images in build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // Set to 'development' for local development or 'production' for production builds
-  entry: './src/index.js', 
+  mode: 'production', // Change to 'production' for final build
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    clean: true, 
-    publicPath: '/', 
+    clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: { loader: 'babel-loader' },
       },
       {
         test: /\.css$/,
@@ -26,24 +25,24 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'], // Already used by HtmlWebpackPlugin
+          },
+        },
+      ],
     }),
   ],
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'public'),
-    },
+    static: { directory: path.resolve(__dirname, 'public') },
     port: 3000,
     hot: true,
     open: true,
     historyApiFallback: true,
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    ],
+    proxy: [{ context: ['/api'], target: 'http://localhost:5000', changeOrigin: true }],
   },
-} 
+};
